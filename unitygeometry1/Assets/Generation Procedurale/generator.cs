3d5e74cb82d83755efad2bdfc1116b09f2ec2d6a
McLovin <mehdixbadaoui@outlook.fr>
2020-11-29 14:49:30 +0100

    can generate multiple buildings
    block of buildings all looking in the same direction (ugly)

---
+++ unitygeometry1/Assets/Generation Procedurale/generator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generator : MonoBehaviour
+{
+    public GameObject building;
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Vector3 new_pos = new Vector3(i, transform.position.y, j);
+                Instantiate(building, new_pos, Quaternion.identity);
+
+            }
+        }
+    }
+
+}
