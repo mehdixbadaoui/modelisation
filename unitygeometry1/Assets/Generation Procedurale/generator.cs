fc4edfbc7d8df61226b93527d05590fd1c81ebbc
McLovin <mehdixbadaoui@outlook.fr>
2020-11-30 00:51:21 +0100

    added blocks

--- unitygeometry1/Assets/Generation Procedurale/generator.cs
+++ unitygeometry1/Assets/Generation Procedurale/generator.cs
@@ -6,13 +6,16 @@ public class generator : MonoBehaviour
     public int width = 10;
     public int height = 10;
 
-    [Range(10f, 100f)]
+    public int blockWidth = 2;
+    public int blockHeight = 2;
+
+    [Range(10f, 500f)]
     public float scale = 20f;
 
     [Range(10f, 100f)]
     public float XOffset = 10f;
 
-    [Range(10f, 100f)]
+    [Range(1f, 100f)]
     public float YOffset = 10f;
 
     float building_height;
@@ -20,14 +23,17 @@ public class generator : MonoBehaviour
     void Start()
     {
         GameObject go;
-        for (int i = 0; i < width; i++)
+        for (int i = (int) -width / 2 , block_w = 0; i < width / 2; i++, block_w++)
         {
-            for (int j = 0; j < height; j++)
+            if (block_w % (blockWidth + 1) == 0) continue;
+            for (int j = (int) - height / 2, block_h = 0; j < height / 2; j++, block_h ++)
             {
+                if (block_h % (blockHeight + 1) == 0) continue;
+
                 building_height = calc_height(i, j);
-                Debug.Log(building_height);
 
                 Vector3 new_pos = new Vector3(i, transform.position.y, j);
+
                 go = Instantiate(building, new_pos, Quaternion.identity);
                 go.transform.SetParent(transform);
 
