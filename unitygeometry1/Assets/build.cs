4f2c62f6730d89b9b264ac734442d2026254c89a
McLovin <mehdixbadaoui@outlook.fr>
2020-11-29 12:14:16 +0100

    can build using building parts

---
+++ unitygeometry1/Assets/build.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class build : MonoBehaviour
+{
+    public GameObject[] bottom;
+    public GameObject[] middle;
+    public GameObject[] top;
+
+    Collider collider;
+    
+    float height_offste;
+    // Start is called before the first frame update
+    void Start()
+    {
+        height_offste = 0;
+        buildBottom();
+        buildMidle();
+        buildTop();
+    }
+
+    private void buildBottom()
+    {
+        Vector3 newPos = transform.position;
+        newPos.y += height_offste;
+
+        GameObject go = bottom[UnityEngine.Random.Range(0, bottom.Length)];
+        Instantiate(go, newPos , Quaternion.identity);
+        //collider = go.AddComponent<Collider>();
+
+        //height_offste += go.GetComponent<Collider>().bounds.size.y;
+        height_offste += go.GetComponentInChildren<MeshRenderer>().bounds.size.y;
+    }
+
+    private void buildMidle()
+    {
+        Vector3 newPos = transform.position;
+        newPos.y += height_offste;
+
+        GameObject go = middle[UnityEngine.Random.Range(0, middle.Length)];
+        Instantiate(go, newPos, Quaternion.identity);
+
+        //collider = go.AddComponent<Collider>();
+
+        //height_offste += go.GetComponent<Collider>().bounds.size.y;
+        height_offste += go.GetComponentInChildren<MeshRenderer>().bounds.size.y;
+    }
+
+    private void buildTop()
+    {
+        Vector3 newPos = transform.position;
+        newPos.y += height_offste;
+
+        GameObject go = top[UnityEngine.Random.Range(0, top.Length)];
+        Instantiate(go, newPos, Quaternion.identity);
+        //collider = go.AddComponent<Collider>();
+
+        //height_offste += go.GetComponent<Collider>().bounds.size.y;
+        height_offste += go.GetComponentInChildren<MeshRenderer>().bounds.size.y;
+    }
+
+}
